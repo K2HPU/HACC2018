a2d1b7ba769eed43f8c3284cfa61993531bb89fb
K2HPU <43024877+K2HPU@users.noreply.github.com>
2018-11-05 09:45:19 -1000

    Initial Baseline SPFX Webpart
    yo @microsoft/sharepoint baseline build

---
+++ src/webparts/matchMaker/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMatchMakerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MatchMakerWebPartStrings' {
+  const strings: IMatchMakerWebPartStrings;
+  export = strings;
+}
